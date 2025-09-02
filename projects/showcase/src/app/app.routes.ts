cefbf618fed1d3fe67f6996227965d794a41b0ed
Manel Solís <manelsp09@gmail.com>
2025-09-02 22:58:50 +0200

    routing fixed

--- projects/showcase/src/app/app.routes.ts
+++ projects/showcase/src/app/app.routes.ts
@@ -2,6 +2,6 @@ import { Routes } from '@angular/router';
 import { Components } from './pages/components/components';
 
 export const routes: Routes = [
-    { path:'components', component: Components },
-    { path: '**', redirectTo: 'components'}
+    { path:'', component: Components },
+    { path: '**', redirectTo: ''}
 ];

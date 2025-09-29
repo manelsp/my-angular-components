fa09ec2a72645376c288347190d124fdf358da80
Manel Solís <manelsp09@gmail.com>
2025-09-29 12:37:35 +0200

    Lib: Sidenav added submenu items

--- projects/my-angular-components/src/lib/components/sidenav/sidenav.ts
+++ projects/my-angular-components/src/lib/components/sidenav/sidenav.ts
@@ -1,16 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'my-components-sidenav',
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule ],
   templateUrl: './sidenav.html',
   styleUrl: './sidenav.css'
 })
 export class MyComponentsSidenav {
   @Input() isOpen = false;
   @Input() title : string = "Menú";
-  @Input() routes: { path: string; label: string }[] = [];
+  @Input() routes: { path?: string; label: string, children?: { path?: string; label: string }[]; }[] = [];
 
   @Output() closed = new EventEmitter<void>();
 
